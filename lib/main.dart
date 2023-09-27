8e502c8b0c5a32cf43552f468af94092926a61e1
Hasssan Ali Saeed <1121hassan@gmail.com>
2023-09-27 07:23:52 -0600

    app_string

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,19 @@ import 'core/router/app_router.dart';
 
 
 void main() {
+  
+  
  
   runApp(const Dalel());
 }
 
 class Dalel extends StatelessWidget {
+  
   const Dalel({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp.router(
       routerConfig: router,
     );
